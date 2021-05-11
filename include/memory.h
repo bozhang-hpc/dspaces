2caf18fcd5451aaaf490faf2a766ddbb13f9b8a3
Zhang690683220 <zhangbo_china@live.cn>
2021-05-11 00:27:38 -0400

    fix API

--- include/memory.h
+++ include/memory.h
@@ -6,6 +6,6 @@
 
 int get_memory_usage_kb(long* vmrss_kb, long* vmsize_kb);
 int get_cluster_memory_usage_kb(long* vmrss_per_process, long* vmsize_per_process, int root, MPI_Comm comm);
-int get_global_memory_usage_kb(long* global_vmrss, long* global_vmsize, int np);
+int get_global_memory_usage_kb(long* global_vmrss, long* global_vmsize, MPI_Comm comm);
 
 #endif
