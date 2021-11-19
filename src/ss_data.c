1925e121a909fecfe604187f43ec03670fa62427
Zhang690683220 <zhangbo_china@live.cn>
2021-11-19 02:04:12 -0500

    debug method 3 async

--- src/ss_data.c
+++ src/ss_data.c
@@ -2474,7 +2474,7 @@ int dht_add_entry_st(struct dht_entry *de, obj_descriptor *odsc)
 
                 bbox_intersect(&odsc->bb, &sub->odsc->bb, &isect);
                 sub->remaining -= bbox_volume(&isect);
-                printf(stderr, "sub_remaining = %d\n", sub->remaining);
+                fprintf(stderr, "sub_remaining = %d\n", sub->remaining);
                 if(sub->remaining == 0) {
                     sub_complete = 1;
                     list_del(&sub->entry);
