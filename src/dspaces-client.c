b7f92032d5bafbeb049e67891359307884cd956a
Zhang690683220 <zhangbo_china@live.cn>
2023-03-29 18:25:07 -0600

    try to take the margo_wait time into gdr_timer && host_timer v3

--- src/dspaces-client.c
+++ src/dspaces-client.c
@@ -4579,7 +4579,7 @@ static int get_data_dual_channel_v2(dspaces_client_t client, int num_odscs,
         }
     }
     
-    double wait_timer, gdr_timer = 0, host_timer = 0;
+    double gdr_timer = 0, host_timer = 0;
     /*  Try to tune the ratio every 2 timesteps
         At timestep (t), if 2nd timer(t) < 0.2ms, means 2nd request(t) finishes no later than the 1st(t).
             Keep the same ratio at (t+1), but swap the request.
@@ -4637,7 +4637,6 @@ static int get_data_dual_channel_v2(dspaces_client_t client, int num_odscs,
             gettimeofday(&end, NULL);
             gdr_timer += (end.tv_sec - start.tv_sec) * 1e3 + (end.tv_usec - start.tv_usec) * 1e-3;
         } else { // host-based path
-            host_timer += wait_timer;
             host_idx = req_idx - num_gdr;
             device_od[host_idx] = obj_data_alloc_cuda(&odsc_tab[req_idx]);
             // H->D async transfer
@@ -4678,7 +4677,7 @@ static int get_data_dual_channel_v2(dspaces_client_t client, int num_odscs,
             ret = ssd_copy_cuda_async(return_od, device_od[host_idx],
                                 &host_stream[host_idx%host_stream_size]);
             gettimeofday(&end, NULL);
-            gdr_timer += (end.tv_sec - start.tv_sec) * 1e3 + (end.tv_usec - start.tv_usec) * 1e-3;
+            host_timer += (end.tv_sec - start.tv_sec) * 1e3 + (end.tv_usec - start.tv_usec) * 1e-3;
         }
         if(ret != dspaces_SUCCESS) {
             fprintf(stderr, "ERROR: (%s): ssd_copy_cuda_async() failed, Err Code: (%d)\n",
